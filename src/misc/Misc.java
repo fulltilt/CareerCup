90026b9c496a74e92d919f7901cdcf092f37b578
Dave D <dave.doria@gmail.com>
2013-07-24 19:36:04 -0700

    added misc.java

---
+++ src/misc/Misc.java
@@ -0,0 +1,532 @@
+package misc;
+
+import java.util.*;
+
+public class Misc {
+    public static boolean binarySearch(int[] array, int start, int end, int value) {
+        if (start > end)
+            return false;
+            
+        int middle = (start + end) / 2;
+        if (array[middle] == value)
+            return true;
+        
+        if (value < array[middle])
+            return binarySearch(array, start, middle - 1, value);
+        else
+            return binarySearch(array, middle + 1, end, value);
+    }
+    
+    /***********************/
+    
+    public static int fibonacci(int n) {	// non-recursive
+/*
+ *     System.out.println(fibonacci(0));
+       System.out.println(fibonacci(1));
+       System.out.println(fibonacci(2));
+       System.out.println(fibonacci(3));
+       System.out.println(fibonacci(4));
+       System.out.println(fibonacci(5));
+       System.out.println(fibonacci(6));
+       System.out.println(fibonacci(7));
+ */        
+        if (n == 0)
+            return 0;
+        if (n == 1)
+            return 1;
+        int previousPrevious = 0;
+        int previous = 1;
+        int sum = 0;
+        for (int i = 1; i < n; i++) {
+            sum = previousPrevious + previous;
+
+            previousPrevious = previous;
+            previous = sum;
+        }
+        
+        return sum;
+    }
+    
+    /***********************/
+    
+    /* CI5 - Duplicates in an Array ranging from 0 to n - 2, one duplicate */
+    public static int findDuplicates1(int[] array) {	// assumes a valid array is passed in
+    	if (array == null || array.length < 2)
+    		return Integer.MIN_VALUE;
+    	
+	    int arraySum = 0;
+	    int actualSum = 0;
+	    for (int i = 0; i < array.length; i++) 
+	        arraySum += array[i];
+	    
+	    for (int i = 0; i < array.length - 1; i++)
+	        actualSum += i;
+	    
+	    actualSum = ((array.length - 2) * (array.length - 1)) >> 1;
+	    return arraySum - actualSum;
+    } 
+    
+    /***********************/
+   
+    /* CI6 - Duplicates in an Array ranging from 0 to n - 1, multiple duplicates */
+    public static int findFirstDuplicate(int[] array) {
+	    HashSet<Integer> hash = new HashSet<Integer>();
+	    for (int i = 0; i < array.length; i++) {
+	        if (array[i] < 0 || array[i] > array.length - 1)
+	            throw new IllegalArgumentException("Number out of bounds!");
+	        if (!hash.contains(array[i]))
+	            hash.add(array[i]);
+	        else {
+	            return array[i];
+	        }
+	    }
+	    
+	    return Integer.MIN_VALUE;
+    }
+    
+    /***********************/
+    
+    /* CI7 - check whether # is in matrix or not */
+    public static boolean inMatrix(int[][] array, int value) {
+        // find the proper row to search
+        int row = 0;
+        boolean result = false;
+        for (row = 0; row < array.length; row++) { // for multidimensional arrays, length is the # of rows
+            int rowLength = array[row].length;
+            if (array[row][rowLength - 1] >= value) {
+                // use binary search to find value (assumption is that the rows are in order)
+                
+                // using built-in binary search
+                //int index = Arrays.binarySearch(array[row], searchValue);
+                //boolean result = index > -1 ? true : false;
+                
+                // manual binary search
+                int start = 0;
+                int end = rowLength - 1;
+                
+                while (start <= end) {
+                    int middle = (start + end) / 2;
+                    int temp = array[row][middle];
+                    if (value == temp) {
+                        result = true;
+                        break;
+                    }
+                    
+                    if (value < temp) 
+                        end = middle - 1;
+                    else
+                        start = middle + 1;
+                }
+            }
+        }
+        
+    	return result;
+    }
+    
+    /***********************/
+    
+    /* CI10 - merging sorted arrays (following deals with arrays of all types of lengths) */ 
+    public static int[] mergeSortedArrays(int[] array1, int[] array2) {
+        int[] array3 = new int[array1.length + array2.length];
+        
+        int index1 = 0;
+        int index2 = 0;
+        int indexMerged = 0;
+
+        while(index1 < array1.length && index2 < array2.length) { 
+            if(array1[index1] <= array2[index2]) 
+                array3[indexMerged++] = array1[index1++]; 
+            else 
+                array3[indexMerged++] = array2[index2++]; 
+        } 
+         
+        // deal with reminder once one of the arrays is empty
+        while (index1 < array1.length)
+            array3[indexMerged++] = array1[index1++];
+        while (index2 < array2.length)
+            array3[indexMerged++] = array2[index2++];
+        
+        return array3;
+        
+        /* from right to left (good when you have to append to the first array so you don't have
+         * to worry about shifting right all the time
+        int[] array1 = {1,3,5,7,9};
+        int[] array2 = {2,4,6,8,10};
+        int[] array3 = new int[array1.length + array2.length];
+        
+        int index1 = array1.length - 1;
+        int index2 = array2.length - 1;
+        int indexMerged = array3.length - 1;
+
+        while(index1 >= 0 && index2 >= 0) { 
+            if(array1[index1] >= array2[index2]) 
+                array3[indexMerged--] = array1[index1--]; 
+            else 
+                array3[indexMerged--] = array2[index2--]; 
+        } 
+         
+        // deal with reminder once one of the arrays is empty
+        while (index1 >= 0)
+            array3[indexMerged--] = array1[index1--];
+        while (index2 >= 0)
+            array3[indexMerged--] = array2[index2--];
+            
+        for (int i = 0; i < array3.length; i++)
+            System.out.println(array3[i]);        
+        */    
+    }
+    
+    /***********************/
+  
+    /* CI27 - Minimum of rotated array */
+    public static int getMinimumOfRotatedArray(int[] array) {
+       //Have two pointers, one at the start and the other at the end. Get the middle element. If it's bigger than 'start', start-middle is in
+       // the bigger part of the array so we can discard that. If it's smaller than end, middle-end is in the lower half so discard that end.
+       // Keep repeating this process until we both pointers are next to each other.
+       // NOTE: this doesn't work 100% of the time when there are duplicates. If that's the case, we resort to a O(n) sequential search
+       
+       int start = 0;
+       int end = array.length - 1;
+       int middle = (start + end) / 2;
+       
+        while (true) {
+            if (array[middle] > array[start]) {
+                start = middle;
+            } else {
+                end = middle;
+            }
+            
+            if (start + 1 == end)
+                return array[end];
+            middle = (start + end) / 2;
+        }
+    }
+    
+    /***********************/
+  
+    /* CI28 - get turning point */
+    public static int getTurningPointIndex(int[] array) {
+        /*
+         * We set a pointer to the start and end of the array. We get the middle element. If the element is greater than the previous index and less
+         * than the next index, we have the turning point. Else if the element is greater than the previous but smaller than the next, discard everything
+         * to the left of middle. Else if the element is less than the prevous but greater than next, discard everything to the right of middle.
+         * NOTE: elements are unique so it's not like 1,2,3,4,5,4,3,2,1
+         */
+       if (array.length == 0)
+           throw new IllegalArgumentException("Array length is zero!");
+       if (array.length == 1 || array.length == 2)
+           return 0;
+           
+       int start = 0;
+       int end = array.length - 1;
+       int middle = (start + end) / 2;
+       
+       while (true) {
+           if (array[middle - 1] < array[middle] && array[middle] > array[middle + 1])
+               return middle;
+           else if (array[middle - 1] < array[middle] && array[middle] < array[middle + 1]) // we're in ascending part of array
+               start = middle;
+           else // we're in descending part of array
+               end = middle;
+           
+           middle = (start + end) / 2;
+        }
+    }
+    
+    /***********************/
+
+    /* CI40 - Write a power function */
+    public static double Power(double base, int exponent) {     
+        if (base == 0 && exponent == 0)
+            throw new IllegalArgumentException("undefined");
+        
+        if (exponent == 0)
+            return 1;
+            
+        int absoluteExponent = exponent;
+        
+        if (exponent < 0)   // to deal with negative exponents
+            absoluteExponent = absoluteExponent * -1;
+            
+        double product = 1.0;    
+        for (int i = 0; i < absoluteExponent; i++)
+            product = product * base;
+        
+        if (exponent < 0)   // x^-2 is equivalent to 1 / x^2
+            product = 1.0 / product;
+            
+        return product;    
+    }
+    
+    /***********************/
+    
+    /* CI45 - Evens before odds */
+    public static void evensBeforeOdds(int[] array) {
+        // using strategy similar to the partition function for quicksort, swap evens and odds 
+        int start = 0;
+        int end = array.length - 1;
+        
+        while (true) {
+            while (array[start] % 2 == 0)
+                ++start;
+                
+            while (array[end] % 2 == 1)
+                --end;
+                
+            if (end > start) {    
+                int temp = array[end];
+                array[end] = array[start];
+                array[start] = temp;
+            } else {
+                break;
+            }
+        }
+    }
+    
+    /***********************/
+    
+    // CI56 - is corresponding sequence for stack. For example, if the pushing sequence is {1, 2, 3, 4, 5}, 
+    // the sequence {4, 5, 3, 2, 1} is a corresponding popping sequence, but {4, 3, 5, 1, 2} is not.
+    public static boolean isCorrespondingSequence(int[] pushingSequence, int[] poppingSequence) {
+        
+        int pushingIndex = 0;
+        int poppingIndex = 0;
+        Stack<Integer> stack = new Stack<Integer>();
+
+        while (pushingIndex < pushingSequence.length || poppingIndex < poppingSequence.length) {
+            if (!stack.empty() && stack.peek() == poppingSequence[poppingIndex]) {
+                stack.pop();
+                ++poppingIndex;
+            } else if (pushingIndex >= pushingSequence.length) { // if it gets here, it means that there's nothing else to push but we can't pop either so sequence is invalid
+                return false;
+            } else { 
+                stack.push(pushingSequence[pushingIndex++]);
+            }
+        }
+        
+        return true;
+    }
+    
+    /***********************/
+    
+    /* CI71 - finds the intersection of two sorted arrays */
+    public static void getIntersection(int[] array1, int[] array2) {        
+        for (int i : array1) {
+            int start = 0;
+            int end = array2.length - 1;
+            
+            // manual binary search
+            while (start <= end) {
+                int middle = (start + end) / 2;
+                int currentValue = array2[middle];
+                if (currentValue == i) {
+                    System.out.print(i + " ");
+                    break;
+                }
+                    
+                if (i > currentValue)
+                    start = middle + 1;
+                else
+                    end = middle - 1;
+            }
+            
+        }
+    }
+    
+    /***********************/
+    
+    /* CI72 - greatest sum of consecutive subarrays */
+    public static int greatestSumOfSubarrays(int[] array) {       
+        // keep on adding #'s but whenever the sum is less than zero, restart at zero
+        int max = 0;
+        int currentSum = 0;
+        
+        for (int i : array) {
+            currentSum += i;
+            if (currentSum > max)
+                max = currentSum;
+                
+            if (currentSum < 0)
+                currentSum = 0;
+        }
+        
+        return max;
+    }
+    
+    /***********************/
+
+    /* CI76 - find the first character in a string that only appears once */
+    public static char findFirstSingleChar(String str) {        
+        int[] ascii = new int[256]; // make ascii hashtable
+        for (int i = 0; i < str.length(); i++) {
+            char currentChar = str.charAt(i);
+            ascii[currentChar]++;
+        }
+
+        for (int i = 0; i < str.length(); i++) {
+            char currentChar = str.charAt(i);
+            if (ascii[currentChar] == 1) 
+                return currentChar;
+        }
+        
+        return ' ';
+    }
+    
+    /***********************/
+    
+    /* CI78 - delete characters contained in the second string from the first string */
+    public static char[] deleteDuplicateChars(String str1, String str2) {
+        char[] char1 = str1.toCharArray();
+        char[] char2 = str2.toCharArray();        
+        
+        int[] ascii = new int[256];
+        for (char c : char2)
+            ascii[c] = 1;
+           
+        // instead of left shifting each index, use 2 pointers of the array and fill in duplicate char with next char    
+        int trailingIndex = 0;    
+        for (int i = 0; i < char1.length; i++) {
+            if (ascii[char1[i]] != 1) {
+                char1[trailingIndex] = char1[i];
+                ++trailingIndex;
+            }
+        }
+        
+        //blank out the rest of the array if needed 
+        while (trailingIndex < char1.length)
+            char1[trailingIndex++] = ' ';
+        
+        return char1;
+    }
+    
+    /***********************/
+    
+    /* 79 - delete all duplicated characters in a string and keep only the first occurrence of each character left */
+    public static char[] deleteDuplicateCharsExceptFirst(String str) {
+        char[] array = str.toCharArray();
+        int[] ascii = new int[256];
+        for (char c : array)
+            ascii[c] = 1;
+            
+        int trailingIndex = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (ascii[array[i]] == 1) {
+                array[trailingIndex++] = array[i];
+                ascii[array[i]] = 0;
+            }
+        }
+        
+        for (int i = trailingIndex; i < array.length; i++)
+            array[i] = ' ';
+        
+        return array;
+    }
+    
+    /***********************/
+
+    /* 80 - are 2 words anagrams */
+    public static boolean areAnagrams(String str1, String str2) {
+        if (str1.length() != str2.length())
+            return false;
+        
+        int[] ascii = new int[256];
+        char[] str1Array = str1.toCharArray();
+        for (char c : str1Array) 
+            ascii[c]++;
+        
+        char[] str2Array = str2.toCharArray();
+        for (char c : str2Array) {
+            if (ascii[c] == 0)  // at least 1 more occurence in str2 than str1
+                return false;
+                
+            ascii[c]--;    
+        }
+        
+        return true;
+    }
+    
+    /***********************/
+    
+    /* 87 - is sum of 2 numbers in an array equal to k */    
+    public static boolean isSumOf2Numbers(int[] array, int k) {
+        int start = 0;
+        int end = array.length - 1;
+        
+        while (start < end) {
+            int sum = array[start] + array[end];
+            if (sum == k)
+                return true;
+            else if (sum > k)
+                --end;
+            else
+                ++start;
+        }
+        
+        return false;
+    }
+    
+    /***********************/
+    
+    /* 88 - check whether it contains three numbers whose sum equals 0 */
+    public static boolean isSumOf3NumbersZero(int[] array) {        
+        if (array.length < 3)
+            throw new IllegalArgumentException("not enough numbers!");
+            
+        for (int i = 0; i < array.length; i++) {
+            if (isSumOf2Numbers2(array, -array[i], i))
+                return true;
+        }
+        
+        return false;
+    }
+    
+    private static boolean isSumOf2Numbers2(int[] array, int k, int index) {
+        int start = 0;
+        int end = array.length - 1;
+        
+        while (start < end) {
+            int sum = array[start] + array[end];
+            if (sum == k)
+                return true;
+            else if (sum > k) {
+                --end;
+                if (end == index)
+                    --end;
+            } else {
+                ++start;
+                if (start == index)
+                    ++start;
+            }
+        }
+        
+        return false;
+    }       
+    
+    /***********************/
+ 
+    /* 90 - Given a positive value s, print all sequences with continuous numbers (with two numbers at least) whose sum is s */
+    public static void printContinuousSequence(int s) {        
+        int startNumber = 1;
+        int sum = startNumber;
+        int currentNumber = startNumber + 1;;
+        while (startNumber < s) {
+            sum += currentNumber;
+            if (sum < s) {
+                ++currentNumber;
+            } else if (sum > s) {
+                ++startNumber;
+                sum = startNumber;
+                currentNumber = startNumber + 1;
+            } else {
+                System.out.println(startNumber + "~" + currentNumber);
+                ++startNumber;
+                sum = startNumber;
+                currentNumber = startNumber + 1;
+            }
+        }
+    }
+    
+    /***********************/
+    
+    /***********************/
+}
---
+++ test/misc/MiscTestCase.java
@@ -0,0 +1,147 @@
+package misc;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MiscTestCase {
+
+	@Test
+	public void test() {
+       int[] array = {1,20,45,78,99,102,189,7777};
+       assertTrue(Misc.binarySearch(array, 0, array.length - 1, 7777));
+       assertFalse(Misc.binarySearch(array, 0, array.length - 1, 2));
+	}
+
+	@Test 
+	public void testFindDuplicates1() {
+		int[] array = {0,3,1,3,2};
+		assertEquals(Misc.findDuplicates1(array), 3);
+	}
+
+	@Test 
+	public void testFindFirstDuplicates() {
+		int[] array = {2,3,1,0,2,5,3};
+		assertEquals(Misc.findFirstDuplicate(array), 2);
+	}	
+	
+	@Test
+	public void testInMatrix() {
+        int[][] array = {{1,3,5},
+                         {7,9,11},
+                         {13,15,17}};
+        assertTrue(Misc.inMatrix(array, 17));
+        assertFalse(Misc.inMatrix(array, 12));
+	}
+	
+	@Test
+	public void testMergeSortedArray() {
+		int[] array = Misc.mergeSortedArrays(new int[]{1,3,5,9,10}, new int[]{2,4,6,7,8});
+		assertEquals(array[0], 1);
+		assertEquals(array[1], 2);
+		assertEquals(array[2], 3);
+		assertEquals(array[3], 4);
+		assertEquals(array[4], 5);
+		assertEquals(array[5], 6);
+		assertEquals(array[6], 7);
+		assertEquals(array[7], 8);
+		assertEquals(array[8], 9);
+		assertEquals(array[9], 10);
+	}
+	
+	@Test
+	public void testGetMinimumOfRotatedArray() {
+		assertEquals(Misc.getMinimumOfRotatedArray(new int[]{3, 4, 5, 1, 2}), 1);
+	}
+	
+	@Test
+	public void testGetTurningPointIndex() {
+		assertEquals(Misc.getTurningPointIndex(new int[]{1, 2, 3, 4, 5, 10, 9, 8, 7, 6}), 5);
+	}	
+
+	@Test
+	public void testPower() {
+		assertEquals(Misc.Power(1,1), 1.0, .01);
+		assertEquals(Misc.Power(2,2), 4.0, .01);
+		assertEquals(Misc.Power(3,3), 27.0, .01);
+		assertEquals(Misc.Power(4,-4), .00390625, .01);
+	}	
+	
+	@Test
+	public void testEvensBeforeOdds() {
+		int[] array = new int[]{1, 5, 3, 4, 2};
+		Misc.evensBeforeOdds(array);
+		assertEquals(array[0], 2);
+		assertEquals(array[1], 4);
+		assertEquals(array[2], 3);
+		assertEquals(array[3], 5);
+		assertEquals(array[4], 1);
+	}
+	
+	@Test
+	public void testIsCorrespondingSequence() {
+	    int[] pushingSequence = {1, 2, 3, 4, 5};
+	    int[] poppingSequence1 = {4, 5, 3, 2, 1};
+	    int[] poppingSequence2 = {4, 3, 5, 1, 2};
+	    assertTrue(Misc.isCorrespondingSequence(pushingSequence, poppingSequence1));
+	    assertFalse(Misc.isCorrespondingSequence(pushingSequence, poppingSequence2));		
+	}
+	
+	@Test
+	public void testGetIntersection() {
+	    int[] a = {1, 4, 7, 10, 13};
+	    int[] b = {1, 3, 5, 7, 9};
+	    int[] c = {1, 4, 7, 10, 13};
+	    Misc.getIntersection(a, b); System.out.println();
+	    Misc.getIntersection(a, c); System.out.println();
+	}
+	
+	@Test
+	public void testGreatestSumOfSubarrays() {
+	    int[] array =  {1, -2, 3, 10, -4, 7, 2,-5};
+	    assertEquals(Misc.greatestSumOfSubarrays(array), 18);		
+	}
+	
+	@Test
+	public void testFindFirstSingleChar() {
+		assertEquals(Misc.findFirstSingleChar("google"), 'l');
+	}
+	
+	@Test
+	public void testDeleteDuplicateChars() {
+        char[] array = Misc.deleteDuplicateChars("We are students", "aeiou");
+        System.out.println(array);
+	}
+	
+	@Test
+	public void testDeleteDuplicateCharsExceptFirst() {
+        char[] array = Misc.deleteDuplicateCharsExceptFirst("google");
+        System.out.println(array);
+	}
+	
+	@Test
+	public void testAreAnagrams() {
+	    assertTrue(Misc.areAnagrams("silent", "listen"));
+	    assertFalse(Misc.areAnagrams("howdy", "doody"));	
+	}
+	
+	@Test 
+	public void testIsSumOf2Numbers() {
+	    int[] array =  {1, 2, 4, 7, 11, 15};
+	    assertTrue(Misc.isSumOf2Numbers(array, 15));
+	    assertTrue(Misc.isSumOf2Numbers(array, 5));
+	    assertTrue(Misc.isSumOf2Numbers(array, 3));
+	    assertFalse(Misc.isSumOf2Numbers(array, 14));
+	}
+	
+	@Test
+	public void testIsSumOf3NumbersZero() {
+		assertFalse(Misc.isSumOf3NumbersZero(new int[]{1, 2, 4, 7, 11, 15}));
+		assertTrue(Misc.isSumOf3NumbersZero(new int[]{1, 2, 4, -6, 11, 15}));		
+	}
+	
+	@Test
+	public void testPrintContinuousSequence() {
+	    Misc.printContinuousSequence(15);        	
+	}
+}
